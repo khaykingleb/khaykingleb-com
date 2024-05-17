a180b37c8c171faf2502a808c54eee83ceee9929
Gleb Khaykin <khaykingleb@gmail.com>
2024-05-17 20:48:50 +0400

    chore: miss to add a new image for the header

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -14,7 +14,7 @@ export const meta: MetaFunction = () => {
 export default function Projects() {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header backgroundImage="/img/van_gogh_wheatfield_with_crows.jpg" />
+      <Header backgroundImage="/img/van_gogh_wheatfield_under_thunderclouds.jpg" />
       <main className="flex flex-grow flex-col items-center justify-center">
         <h1 className="text-center text-3xl font-bold">Coming soon...</h1>
       </main>
