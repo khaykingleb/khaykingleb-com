bdcd0ade6ebdc1b794a3824428123a184ebe3cf2
Gleb Khaykin <khaykingleb@gmail.com>
2024-05-19 12:21:48 +0400

    chore: update intro

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,8 +14,8 @@ export default function Index() {
           <p className="mb-2">
             Welcome! I'm Gleb, a full-stack developer with a focus on DevOps and
             ML/MLOps engineering. Or, in simpler terms, I code things and
-            occasionally break them... (Don't worry, I fix them in staging or QA
-            in the end!)
+            occasionally break them... (Don't worry, I fix them after hitting
+            staging or QA in the end!)
           </p>
           <p className="mb-2">
             Early on, I had thoughts of being a software engineer, investment
@@ -29,15 +29,15 @@ export default function Index() {
             on-premise, and have a solid track record of boosting model
             performance and cutting costs. Outside of work, I enjoy hitting the
             weights at the gym, striving to look like a fitness trainer without
-            actually being one (yet! Who knows, maybe, Devin's gonna make it,
-            lol).
+            actually being one. At least, yet! Who knows, maybe, Devin's gonna
+            make it, lol.
           </p>
           <p className="mb-2">
             Here, you’ll find a collection of resources I’ve found valuable.
             Dive in, and I hope you discover some tools and materials useful for
-            yourself. Also, if you know Russian, check out my Telegram channel
-            in the footer below, it's where I share thoughts and materials in a
-            more unstructured and freestyle format.
+            yourself. Also, you can check out my Telegram channel in the footer
+            below; it's where I share thoughts and materials in a more
+            unstructured and freestyle format.
           </p>
         </div>
       </div>
