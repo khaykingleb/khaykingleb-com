a2f352c584209604c6dd8ea7a860ec10d88890ed
Gleb Khaykin <khaykingleb@gmail.com>
2024-09-03 15:24:31 +0400

    refactor: :hammer:

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -22,24 +22,25 @@ export default function Index() {
       <div className="grow">
         <div className="font-eb-garamond-light mx-auto max-w-lg text-pretty text-center text-sm sm:text-base">
           <p className="mb-2">
-            Hi there! I'm Gleb, a full-stack developer specializing in DevOps
-            and MLOps engineering. My background includes distributed systems,
-            speech processing, speech synthesis, natural language processing,
-            and finance. When I'm not coding, you'll likely find me at the gym,
-            lifting weights.
+            Hi there! I&apos;m Gleb, a full-stack developer specializing in
+            DevOps and MLOps engineering. My background includes distributed
+            systems, speech processing, speech synthesis, natural language
+            processing, and finance. When I&apos;m not coding, you&apos;ll
+            likely find me at the gym, lifting weights.
           </p>
           <p className="mb-2">
-            I've graduated summa cum laude from the Higher School of Economics,
-            where I studied both Computer Science and Finance. This dual
-            background helps me understand both the technical and business
+            I&apos;ve graduated summa cum laude from the Higher School of
+            Economics, where I studied both Computer Science and Finance. This
+            dual background helps me understand both the technical and business
             aspects of projects.
           </p>
           <p className="mb-2">
-            On this site, you'll find a collection of resources that I've found
-            valuable. Feel free to explore, and I hope you discover some useful
-            tools and materials! You can also find links to my Telegram channel
-            (in Russian) and X profile (in English) in the footer below. It's
-            where I share my thoughts and materials in a more freestyle format.
+            On this site, you&apos;ll find a collection of resources that
+            I&apos;ve found valuable. Feel free to explore, and I hope you
+            discover some useful tools and materials! You can also find links to
+            my Telegram channel (in Russian) and X profile (in English) in the
+            footer below. It&apos;s where I share my thoughts and materials in a
+            more freestyle format.
           </p>
         </div>
       </div>
