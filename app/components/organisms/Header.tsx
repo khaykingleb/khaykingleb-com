304c9cb36ff59275b84029d022d7090dc2c463b0
Gleb Khaykin <khaykingleb@gmail.com>
2024-09-22 03:43:48 +0400

    refactor: :hammer:

--- app/components/organisms/Header.tsx
+++ app/components/organisms/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 
 export const Header = ({ backgroundImage }: HeaderProps) => {
   return (
-    <div className="navbar relative h-24 overflow-hidden">
+    <header className="relative flex h-16 items-center px-4 sm:px-6 lg:px-8">
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
@@ -14,14 +14,14 @@ export const Header = ({ backgroundImage }: HeaderProps) => {
         }}
       ></div>
       <div className="absolute inset-0 bg-gradient-to-b from-black/20 to-black/20"></div>
-      <div className="relative mx-4 flex-1">
+      <div className="relative z-10 mx-auto flex w-full max-w-[700px] items-center justify-between">
         <span className="font-eb-garamond-black text-lg text-white sm:text-2xl">
-          khaykingleb
+          ~/khaykingleb
         </span>
+        <nav>
+          <Menu />
+        </nav>
       </div>
-      <div className="relative flex-none">
-        <Menu />
-      </div>
-    </div>
+    </header>
   );
 };
