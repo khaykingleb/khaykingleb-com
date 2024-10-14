f49cda7b13dd97ac0e09bcae8814e883630c0fe8
Gleb Khaykin <khaykingleb@gmail.com>
2024-10-14 16:44:39 +0400

    refactor: loading spinner atom

---
+++ app/components/atoms/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+export const LoadingSpinner = () => {
+  return (
+    <div className="flex min-h-[calc(100vh-200px)] items-center justify-center">
+      <div className="loading loading-infinity loading-md"></div>
+    </div>
+  );
+};
