c741fc3c0cdd7245786d112702bd10f8be26ce7e
Gleb Khaykin <khaykingleb@gmail.com>
2024-10-30 22:19:48 +0400

    feat(font): use gill-sans

--- app/components/atoms/Copyright.tsx
+++ app/components/atoms/Copyright.tsx
@@ -1,6 +1,6 @@
 export const Copyright = () => {
   return (
-    <div className="mb-2 text-center">
+    <div className="font-gill-sans-regular mb-2 text-center">
       <p>&copy; 2024 Gleb Khaykin</p>
     </div>
   );
