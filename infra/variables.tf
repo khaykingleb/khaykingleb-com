19be6853288db1c1d6389cc7953a8d5ca89c374e
Gleb Khaykin <khaykingleb@gmail.com>
2024-12-26 01:52:56 +0700

    feat(infra): add vercel env variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,21 @@ variable "vercel_api_token" {
   type        = string
   sensitive   = true
 }
+
+variable "supabase_url" {
+  description = "The Supabase URL"
+  type        = string
+  sensitive   = true
+}
+
+variable "supabase_service_role_key" {
+  description = "The Supabase service role key"
+  type        = string
+  sensitive   = true
+}
+
+variable "supabase_anon_key" {
+  description = "The Supabase anon key"
+  type        = string
+  sensitive   = true
+}
